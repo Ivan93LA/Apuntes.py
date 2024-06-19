e06748c6b644d4c66ab78581597425db73d3c7fd
Ivan93LA <ivan.inln17@gmail.com>
2024-06-19 23:16:45 +0200

    Modificación archivos

--- 12_exceptions.py
+++ 12_exceptions.py
@@ -3,7 +3,7 @@
 
 numberOne = 5
 numberTwo = 10
-#numberTwo = "10" #Python no entiende el error con esta excepción
+numberTwo = "10" #Python no entiende el error con esta excepción
 
 
 #Ejemplo de try except
@@ -25,4 +25,24 @@ else:
     print("La ejecucción continua correctamente") 
 finally:
     print("La ejecución continúa")
+
+#Capura de excepciones por tipo
+try:
+    print (numberOne + numberTwo)
+    print("No se ha producido un error")
+except ValueError:
+    #Se ejecuta si se produce una excepcion
+    print ("Se ha producido un ValueError")
+except TypeError:
+    print ("Se ha producido un TypeError")
+
+#Captura de la información de la excepción
+try:
+    print (numberOne + numberTwo)
+    print("No se ha producido un error")
+except ValueError as error:
+    #Se ejecuta si se produce una excepcion
+    print (error)
+except Exception as exception:
+   print (exception)
 
