b7efe656d674d917d6da430ce72acd55af69d881
Ivan93LA <ivan.inln17@gmail.com>
2024-06-20 22:54:37 +0200

    CursoApi

---
+++ Backend/type_hints.py
@@ -0,0 +1,15 @@
+###Tyoe hints
+
+mi_variable = "mi variable string"
+print(mi_variable)
+print(type(mi_variable))
+#tipado dinámico, se puede cambiar durante su ejecucción
+mi_variable = 5
+print(mi_variable)
+
+my_typed_variable: str = "My string typed String variable"
+print(my_typed_variable)
+
+my_typed_variable = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
